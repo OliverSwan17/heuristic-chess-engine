d9d534416af9d126b97e2f214b81cfeb6d53783a
whalda <whiskyhotel123@gmail.com>
2024-11-18 15:10:49 +1100

    Have to play around with some new parameters or get some more data in. It's extremely accurate on the training data, but abysmal at actual data from desktop. Will have to test for overfitting etc

--- src/board_parser/board_parser.py
+++ src/board_parser/board_parser.py
@@ -109,7 +109,7 @@ class ChessboardSquares():
             plt.imshow(img, cmap="gray")
         plt.show()
 
-
+"""
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
@@ -146,6 +146,55 @@ class NeuralNetwork(nn.Module):
         x = self.dropout(x)  # Apply dropout during training
         x = self.fc2(x)
         return x
+"""
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+
+        # First convolutional block
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=5, padding=2)  # Increased number of filters and kernel size
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)  # Increased filters
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Second convolutional block
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)  # Increased filters
+        self.bn3 = nn.BatchNorm2d(256)
+        
+        # Third convolutional block (optional)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)  # Adding more layers
+        self.bn4 = nn.BatchNorm2d(512)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(512 * 4 * 4, 1024)  # Increased number of neurons
+        self.fc2 = nn.Linear(1024, 512)  # Adding another fully connected layer
+        self.fc3 = nn.Linear(512, 26)  # Output layer for 26 classes (pieces and squares)
+
+        # Dropout layer to prevent overfitting
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        # Convolutional Layers with ReLU and Pooling
+        x = torch.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = torch.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = torch.relu(self.bn3(self.conv3(x)))
+        x = self.pool(x)
+        x = torch.relu(self.bn4(self.conv4(x)))  # Add the fourth convolutional block
+        x = self.pool(x)
+
+        # Flatten the output for fully connected layers
+        x = x.view(-1, 512 * 4 * 4)  # Adjust this based on input size and layer depth
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)  # Apply dropout during training
+        x = torch.relu(self.fc2(x))  # Additional fully connected layer
+        x = self.fc3(x)  # Final output layer
+        return x
+
+
 
 
 def take_screenshot(save: bool = False):
@@ -238,7 +287,7 @@ def train():
 
 
     dataset = ChessboardSquares()
-    train_loader = DataLoader(dataset, batch_size = 40, shuffle = True)
+    train_loader = DataLoader(dataset, batch_size = 64, shuffle = True)
 
     model = NeuralNetwork() # TOY WITH THESE VALUES
     optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay = 1e-5)
